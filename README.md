d4cbea242ac2515509cb5eef09e313b5b3089fb4
Miki Tebeka <miki@353solutions.com>
2022-12-26 16:03:56 +0200

    example & separate examples

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # stats - Simple Statistics Library for Go
+[![CI](https://github.com/tebeka/stats/actions/workflows/go.yml/badge.svg)](https://github.com/tebeka/stats/actions/workflows/go.yml)
 
+See [example_test.go](example_test.go) for example usage.
--- example_test.go
+++ example_test.go
@@ -2,42 +2,113 @@ package stats
 
 import "fmt"
 
-func ExampleSats() {
+func ExampleArgMin() {
+	v := []int{3, 1, 4, 2}
+	i, err := ArgMin(v)
+	fmt.Printf("argmin: %v (%v)\n", i, err)
+
+	// Output:
+	// argmin: 1 (<nil>)
+}
+
+func ExampleMin() {
 	v := []int{3, 1, 4, 2}
 	m, err := Min(v)
-	fmt.Printf("min : %v (%v)\n", m, err)
-	m, err = Max(v)
-	fmt.Printf("max : %v (%v)\n", m, err)
-	fmt.Println("sum :", Sum(v))
+	fmt.Printf("min: %v (%v)\n", m, err)
+
+	// Output:
+	// min: 1 (<nil>)
+}
+
+func ExampleArgMax() {
+	v := []int{3, 1, 4, 2}
+	i, err := ArgMax(v)
+	fmt.Printf("argmax: %v (%v)\n", i, err)
+
+	// Output:
+	// argmax: 2 (<nil>)
+}
+
+func ExampleMax() {
+	v := []int{3, 1, 4, 2}
+	m, err := Max(v)
+	fmt.Printf("max: %v (%v)\n", m, err)
+
+	// Output:
+	// max: 4 (<nil>)
+}
+
+func ExampleSum() {
+	v := []int{3, 1, 4, 2}
+	fmt.Println("sum:", Sum(v))
+
+	// Output:
+	// sum: 10
+}
+
+func ExampleProd() {
+	v := []int{3, 1, 4, 2}
 	fmt.Println("prod:", Prod(v))
+
+	// Output:
+	// prod: 24
+}
+
+func ExampleMean() {
+	v := []int{3, 1, 4, 2}
 	f, err := Mean(v)
 	fmt.Printf("mean: %v (%v)\n", f, err)
+
+	// Output:
+	// mean: 2.5 (<nil>)
+}
+
+func ExampleMedian() {
+	v := []int{3, 1, 2}
+	f, err := Median(v)
+	fmt.Printf("median: %v (%v)\n", f, err)
+
+	v = append(v, 4)
 	f, err = Median(v)
-	fmt.Printf("med : %v (%v)\n", f, err)
-	f, err = Var(v)
-	fmt.Printf("var : %v (%v)\n", f, err)
-	f, err = Std(v)
-	fmt.Printf("std : %v (%v)\n", f, err)
-	m, err = Dot(v, v)
+	fmt.Printf("median: %v (%v)\n", f, err)
+
+	// Output:
+	// median: 2 (<nil>)
+	// median: 2.5 (<nil>)
+}
+
+func ExampleVar() {
+	v := []int{3, 1, 4, 2}
+	f, err := Var(v)
+	fmt.Printf("var: %v (%v)\n", f, err)
+
+	// Output:
+	// var: 1.25 (<nil>)
+}
+
+func ExampleStd() {
+	v := []int{3, 1, 4, 2}
+	f, err := Std(v)
+	fmt.Printf("std: %v (%v)\n", f, err)
+
+	// Output:
+	// std: 1.118033988749895 (<nil>)
+}
+
+func ExampleDot() {
+	v := []int{3, 1, 4, 2}
+	m, err := Dot(v, v)
 	fmt.Printf("dot : %v (%v)\n", m, err)
-	i, err := ArgMin(v)
-	fmt.Printf("amin: %v (%v)\n", i, err)
-	i, err = ArgMax(v)
-	fmt.Printf("amax: %v (%v)\n", i, err)
-	mod, err := Mode([]rune{'h', 'e', 'l', 'l', 'o'})
+
+	// Output:
+	// dot : 30 (<nil>)
+}
+
+func ExampleMode() {
+	v := []rune{'h', 'e', 'l', 'l', 'o'}
+	mod, err := Mode(v)
 	fmt.Printf("mode: %c (%v)\n", mod, err)
 
 	// Output:
-	// min : 1 (<nil>)
-	// max : 4 (<nil>)
-	// sum : 10
-	// prod: 24
-	// mean: 2.5 (<nil>)
-	// med : 2.5 (<nil>)
-	// var : 1.25 (<nil>)
-	// std : 1.118033988749895 (<nil>)
-	// dot : 30 (<nil>)
-	// amin: 1 (<nil>)
-	// amax: 2 (<nil>)
 	// mode: l (<nil>)
 }
