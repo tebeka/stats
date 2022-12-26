b30f1c6789ffc85a41106f0c101bef7733d0bc07
Miki Tebeka <miki@353solutions.com>
2022-12-26 16:07:25 +0200

    ~float32/64

--- stats.go
+++ stats.go
@@ -14,7 +14,7 @@ var (
 type Ordered interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
-		float32 | float64
+		~float32 | ~float64
 }
 
 // ArgMin returns the index of minimal value in values
